c5d2721c44dade91190ab3e676af60ac9bc15790
mohini modi <mohini.modi@resmed.com>
2021-03-05 13:28:04 -0800

    learn mat

---
+++ CommonLinuxTasks/commands.bash
@@ -0,0 +1,20 @@
+# To create new file 
+# to create new directory
+# Remane file 
+# Delete file
+# delete multiple file with similar names
+# delete folder
+
+#file manupulation 
+pwd
+mkdir linuxtuto
+ls
+mkdir -p linuxtuto # to make it as parent directory 
+mkdir -vp linuxtuto # to have visibility in details of action taken for mkdir
+
+#to remove
+rmdir linuxtuto
+rmdir -pv linuxtuto # to have visibility in details of action taken for mkdir
+
+# To create file 
+touch newfile # touch is use to create, modify
